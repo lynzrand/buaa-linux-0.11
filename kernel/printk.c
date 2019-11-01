4722a4d22d6b3e710847f05e9b02aea54f40efa5
Rynco Maekawa <lynzrand@outlook.com>
2019-11-01 21:46:20 +0800

    Add printk

--- kernel/printk.c
+++ kernel/printk.c
@@ -16,7 +16,7 @@
 
 static char buf[1024];
 
-extern int vsprintf(char * buf, const char * fmt, va_list args);
+extern int vsprintf(char *buf, const char *fmt, va_list args);
 
 int printk(const char *fmt, ...)
 {
@@ -24,18 +24,79 @@ int printk(const char *fmt, ...)
 	int i;
 
 	va_start(args, fmt);
-	i=vsprintf(buf,fmt,args);
+	i = vsprintf(buf, fmt, args);
 	va_end(args);
 	__asm__("push %%fs\n\t"
-		"push %%ds\n\t"
-		"pop %%fs\n\t"
-		"pushl %0\n\t"
-		"pushl $buf\n\t"
-		"pushl $0\n\t"
-		"call tty_write\n\t"
-		"addl $8,%%esp\n\t"
-		"popl %0\n\t"
-		"pop %%fs"
-		::"r" (i):"ax","cx","dx");
+			"push %%ds\n\t"
+			"pop %%fs\n\t"
+			"pushl %0\n\t"
+			"pushl $buf\n\t"
+			"pushl $0\n\t"
+			"call tty_write\n\t"
+			"addl $8,%%esp\n\t"
+			"popl %0\n\t"
+			"pop %%fs" ::"r"(i)
+			: "ax", "cx", "dx");
 	return i;
+}
+
+#include <linux/sched.h>
+#include <sys/stat.h>
+
+static char logbuf[1024];
+int fprintk(int fd, const char *fmt, ...)
+{
+	va_list args;
+	int count;
+	struct file *file;
+	struct m_inode *inode;
+
+	va_start(args, fmt);
+	count = vsprintf(logbuf, fmt, args);
+	va_end(args);
+	/* 如果输出到stdout或stderr，直接调用sys_write即可 */
+	if (fd < 3)
+	{
+		__asm__("push %%fs\n\t"
+				"push %%ds\n\t"
+				"pop %%fs\n\t"
+				"pushl %0\n\t"
+				/* 注意对于Windows环境来说，是_logbuf,下同 */
+				"pushl $logbuf\n\t"
+				"pushl %1\n\t"
+				/* 注意对于Windows环境来说，是_sys_write,下同 */
+				"call sys_write\n\t"
+				"addl $8,%%esp\n\t"
+				"popl %0\n\t"
+				"pop %%fs" ::"r"(count),
+				"r"(fd)
+				: "ax", "cx", "dx");
+	}
+	else
+	/* 假定>=3的描述符都与文件关联。事实上，还存在很多其它情况，这里并没有考虑。*/
+	{
+		/* Work only if we have process 1 (init) up and working */
+		if (last_pid > 1)
+		{
+			/* 从进程1的文件描述符表中得到文件句柄 */
+			if (!(file = task[1]->filp[fd]))
+				return 0;
+			inode = file->f_inode;
+
+			__asm__("push %%fs\n\t"
+					"push %%ds\n\t"
+					"pop %%fs\n\t"
+					"pushl %0\n\t"
+					"pushl $logbuf\n\t"
+					"pushl %1\n\t"
+					"pushl %2\n\t"
+					"call file_write\n\t"
+					"addl $12,%%esp\n\t"
+					"popl %0\n\t"
+					"pop %%fs" ::"r"(count),
+					"r"(file), "r"(inode)
+					: "ax", "cx", "dx");
+		}
+	}
+	return count;
 }
