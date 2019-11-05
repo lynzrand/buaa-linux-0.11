66091cdf53e57d50158b044f5fdef257d47568a8
Rynco Maekawa <lynzrand@outlook.com>
2019-11-05 18:26:56 +0800

    format all files to avoid further formatting in chaps

--- lib/_exit.c
+++ lib/_exit.c
@@ -9,5 +9,5 @@
 
 volatile void _exit(int exit_code)
 {
-	__asm__("int $0x80"::"a" (__NR_exit),"b" (exit_code));
+	__asm__("int $0x80" ::"a"(__NR_exit), "b"(exit_code));
 }
--- lib/wait.c
+++ lib/wait.c
@@ -5,12 +5,12 @@
  */
 
 #define __LIBRARY__
-#include <unistd.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
-_syscall3(pid_t,waitpid,pid_t,pid,int *,wait_stat,int,options)
+_syscall3(pid_t, waitpid, pid_t, pid, int *, wait_stat, int, options)
 
-pid_t wait(int * wait_stat)
+	pid_t wait(int *wait_stat)
 {
-	return waitpid(-1,wait_stat,0);
+	return waitpid(-1, wait_stat, 0);
 }
